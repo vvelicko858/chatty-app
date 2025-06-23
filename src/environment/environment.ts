c579e6b4a90f0b5b8e818a8bd192aa2572672e8e
Ivan Vialichka <vvelicko858@gmail.com>
2025-06-23 23:30:11 +0300

    registration form

---
+++ src/environment/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBb91FfLGOuEyG6t0lBh9e2V86BzKjdUyM",
+    authDomain: "chatty-app-4b5e9.firebaseapp.com",
+    projectId: "chatty-app-4b5e9",
+    storageBucket: "chatty-app-4b5e9.firebasestorage.app",
+    messagingSenderId: "480179803508",
+    appId: "1:480179803508:web:74a6865557d5a168210821",
+    measurementId: "G-EZBR9NP799"
+  }
+};
